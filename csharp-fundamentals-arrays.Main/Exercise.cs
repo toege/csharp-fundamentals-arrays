03f03f8b8882d1722e857521dc7321ccf5355a36
uerbzr <nigel.sibbert@gmail.com>
2023-03-15 14:41:18 +0000

    Add project files.

---
+++ csharp-fundamentals-arrays.Main/Exercise.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_fundamentals_arrays.Main
+{
+    public class Exercise
+    {
+        // The block of code below is a method definition. So far, you've been using methods created by other people
+        // such as .length() and .charAt(n)
+        // From this exercise on, you'll be writing code inside methods very often! Methods are a container for a specific
+        // piece of logic that the class can run. For now, just implement your solutions between the comments that look like:
+        // WRITE YOUR CODE BETWEEN THIS LINE...
+        // ... AND THIS LINE
+        // See the example method below
+        public void example()
+        {
+            //TODO: 1. Print the word "Hello"
+            // WRITE YOUR CODE BETWEEN THIS LINE...
+            Console.WriteLine("Hello");
+            // ... AND THIS LINE
+        }
+
+        public int[] one()
+        {
+            throw new NotImplementedException();
+            int[] numbers = { 42, 13, 17, 91 };
+
+            // 1. Values contained in an array are each stored at a unique numeric index, starting from 0 ascending in order.
+            // E.g. The first value is at index 0, the second at index 1, the third at index 3.
+            // Using an index, change the number 17 in the numbers array to 68
+            // WRITE YOUR CODE BETWEEN THIS LINE...
+
+
+
+            // ... AND THIS LINE
+
+            return numbers;
+        }
+
+        public String two()
+        {
+            throw new NotImplementedException();
+            String[] teachers = { "Nathan", "Ed", "Dave", "Carlo", "Lewis", "Jules", "John", "Chris", "Nigel" };
+
+            //TODO: 2. Using an array index, change the value of the teacher variable below to be the fourth
+            // teacher contained in the teachers array
+            // WRITE YOUR CODE BETWEEN THIS LINE...
+
+            String teacher = "";
+
+            // ... AND THIS LINE
+
+            return teacher;
+        }
+
+        public String[] three()
+        {
+            throw new NotImplementedException();
+            //TODO: 3. Create a string array named cars that contains three names of car manufacturers: Audi, BMW and Dodge
+            // WRITE YOUR CODE BETWEEN THIS LINE...
+
+
+
+            // ... AND THIS LINE
+
+            //return cars;
+        }
+
+        public int four()
+        {
+            throw new NotImplementedException();
+            int[] numbers = { 42, 13, 17, 91 };
+
+            // TODO 4. Using array indices, set the value of the result variable below to the sum of every number in the numbers array
+            // WRITE YOUR CODE BETWEEN THIS LINE...
+
+            int result = 0;
+
+            // ... AND THIS LINE
+
+            return result;
+        }
+
+        public float[] five()
+        {
+            throw new NotImplementedException();
+            //TODO: 5. Create an array called floats that contains 3 floating point numbers: 9.62, 23.17 and 3.14
+            // WRITE YOUR CODE BETWEEN THIS LINE...
+
+            float[] floats;
+
+
+            // ... AND THIS LINE
+
+            return floats;
+        }
+    }
+}
---
+++ csharp-fundamentals-arrays.Test/ExerciseTest.cs
@@ -0,0 +1,53 @@
+﻿using csharp_fundamentals_arrays.Main;
+using NUnit.Framework;
+
+namespace csharp_fundamentals_arrays.Test
+{
+    public class ExerciseTest
+    {
+        Exercise exercise;
+
+        public ExerciseTest()
+        {
+            this.exercise = new Exercise();
+        }
+
+        [Test]
+        public void shouldChange17To68()
+        {
+            Assert.AreEqual(68, this.exercise.one()[2]);
+        }
+
+        [Test]
+        public void shouldBeCarlo()
+        {
+            Assert.AreEqual("Carlo", this.exercise.two());
+        }
+
+        [Test]
+        public void shouldContainCarManufacturers()
+        {
+            String[] cars = this.exercise.three();
+            Assert.AreEqual(3, cars.Length);
+            Assert.AreEqual("Audi", cars[0]);
+            Assert.AreEqual("BMW", cars[1]);
+            Assert.AreEqual("Dodge", cars[2]);
+        }
+
+        [Test]
+        public void shouldBe163()
+        {
+            Assert.AreEqual(163, this.exercise.four());
+        }
+
+        [Test]
+        public void fiveShouldBeArrayOfFloats()
+        {
+            float[] floats = this.exercise.five();
+            Assert.AreEqual(3, floats.Length);
+            Assert.AreEqual(9.62f, floats[0]);
+            Assert.AreEqual(23.17f, floats[1]);
+            Assert.AreEqual(3.14f, floats[2]);
+        }
+    }
+}
